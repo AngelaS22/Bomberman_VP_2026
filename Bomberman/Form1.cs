9aa7c341470ebae9f322be666f3c529ae6d9c748
unknown <angela.stojcevska99@gmail.com>
2026-06-23 13:22:46 +0200

    Add NacrtajMapa class

---
+++ Bomberman/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bomberman
+{
+    public partial class Form1 : Form
+    {
+        NacrtajMapa nacrtajMapa;
+        public Form1()
+        {
+            InitializeComponent();
+            this.ClientSize = new Size(600, 520);
+            nacrtajMapa = new NacrtajMapa();
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Draw(e.Graphics);
+        }
+        private void Draw(Graphics g)
+        {
+            for(int i=0; i<nacrtajMapa.Rows; i++)
+            {
+                for(int j=0; j<nacrtajMapa.Columns; j++)
+                {
+                    if (nacrtajMapa.Mapa[i, j] == 1)
+                    {
+                        g.FillRectangle(Brushes.Gray, j * nacrtajMapa.GoleminaKelija, i * nacrtajMapa.GoleminaKelija, nacrtajMapa.GoleminaKelija, nacrtajMapa.GoleminaKelija);
+                    }
+                    else
+                    {
+                        g.FillRectangle(Brushes.Green, j * nacrtajMapa.GoleminaKelija, i * nacrtajMapa.GoleminaKelija, nacrtajMapa.GoleminaKelija, nacrtajMapa.GoleminaKelija);
+                    }
+                }
+            }
+        }
+
+      
+    }
+}
---
+++ Bomberman/NacrtajMapa.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bomberman
+{
+    internal class NacrtajMapa
+    {
+        public int Rows = 13;
+        public int Columns = 15;
+        public int GoleminaKelija = 40;
+
+        public int[,] Mapa;
+
+        public NacrtajMapa()
+        {
+            Mapa = new int[Rows, Columns];
+            GenerirajMapa();
+        }
+        private void GenerirajMapa()
+        {
+            for(int i=0; i<Rows; i++)
+            {
+                for(int j=0; j<Columns; j++)
+                {
+                    if(i==0 || j==0 || i==Rows-1 || j == Columns - 1)
+                    {
+                        Mapa[i, j] = 1;
+                    }
+                    else if(i%2==0 && j % 2 == 0)
+                    {
+                        Mapa[i, j] = 1;
+                    }
+                    else
+                    {
+                        Mapa[i, j] = 0;
+                    }
+                }
+            }
+        }
+    }
+}
